04be5d3fe0014d205a1cc50e572b009a68a04fb8
Pierre-Arthur DEMENGEL <124669857+pierrearthurdemengel@users.noreply.github.com>
2024-01-31 20:22:42 +0100

    Expérience pro ok

--- src/Experience.js
+++ src/Experience.js
@@ -1,15 +1,16 @@
-
 function Experience() {
-    return (
-      <section id="experience">
-        <h2>Expérience Professionnelle</h2>
+  return (
+    <section id="experience">
+      <h2>Expérience Professionnelle</h2>
+      <div className="card">
+        <img src="/logo-csp.png" alt="Logo CSP" />
         <ul>
-          <li>Stage en développement web et mobile - Création site pro, Bruxelles (2023)</li>
-          <li>Equipier en salle d'escalade - HAPIK, Strasbourg (2022)</li>
+          <li>Stage en développement web et mobile<br></br>
+            Création site pro, Bruxelles (2023)</li>
         </ul>
-      </section>
-    );
-  }
-  
-  export default Experience;
-  
+      </div>
+    </section>
+  );
+}
+
+export default Experience;
